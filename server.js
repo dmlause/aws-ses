f2f71e614f2c773eab30d8093741518f3cf08978
Derek Lause <derek.m.lause@gmail.com>
2019-01-24 20:02:30 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const aws = require('aws-sdk');
+const express = require('express');
+const dotenv = require('dotenv');
+const port = process.env.PORT || 3001;
+
+var app = express();
+
+// Load the environment variables from .env
+dotenv.load();
+
+// Configure AWS
+aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'us-west-2'
+});
+
+app.get('/send-email', (req, res) => {
+
+    // Set the params for sending an email
+    const params = {
+        Destination: {
+            ToAddresses: ['contactdlause@gmail.com']
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Charset: "UTF-8",
+                    Data: req.query.subject,
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: req.query.body,
+            }
+        },
+        Source: 'contactdlause@gmail.com',
+    };
+
+    const sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+    // Send the Email
+    sendPromise.then((data) => {
+        res.status(200).json({status: 'Email sent successfully.'});
+    }).catch((error) => {
+        res.status(500).json({status: 'Email did not send. Error: ' + error.stack });
+    });
+});
+
+app.listen(port, () => {
+    console.log("Contact-Page Email Server listening on port " + port + "...");
+});
